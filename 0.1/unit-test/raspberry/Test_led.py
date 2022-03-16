9f527c16636a5030ea07631781de944f466bcb42
Yarik9008 <iaroslav.muhatdinov@yandex.ru>
2022-03-16 21:15:19 +0300

    led

---
+++ 0.1/unit-test/raspberry/Test_led.py
@@ -0,0 +1,17 @@
+import time
+from adafruit_servokit import ServoKit
+
+# Set channels to the number of servo channels on your kit.
+# 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
+kit = ServoKit(channels=16)
+
+canal = 8
+
+
+kit.servo[canal].angle = 180
+time.sleep(5)
+kit.servo[canal].angle = 0
+time.sleep(5)
+kit.servo[canal].angle = 90
+print('final')
+
