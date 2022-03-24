c6d83df25a6ebdf43cc2e7e692391428dc08cc6e
Yarik9008 <iaroslav.muhatdinov@yandex.ru>
2022-03-24 23:33:09 +0400

    fckglh

--- 0.1/controll-post/main_controll_post_linux.py
+++ 0.1/controll-post/main_controll_post_linux.py
@@ -91,7 +91,7 @@ class ServerMainPult:
             self.PORT = 1112
         else:
             self.HOST = '192.168.88.5'
-            self.PORT = 1237
+            self.PORT = 2267
             
             
         # настройка сервера
