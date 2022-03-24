e9d2d5fe570ac6d0a255a9a434ba019470a57dd0
Yarik9008 <iaroslav.muhatdinov@yandex.ru>
2022-03-25 01:07:49 +0400

    dddd

--- 0.1/controll-post/main_controll_post_linux.py
+++ 0.1/controll-post/main_controll_post_linux.py
@@ -91,7 +91,7 @@ class ServerMainPult:
             self.PORT = 1121
         else:
             self.HOST = '192.168.88.5'
-            self.PORT = 2272
+            self.PORT = 2273
             
             
         # настройка сервера
