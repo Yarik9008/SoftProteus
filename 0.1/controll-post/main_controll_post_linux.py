26bacd64bc8076037eed2e54c70e593a79eadd2a
Yarik9008 <iaroslav.muhatdinov@yandex.ru>
2022-03-24 23:35:27 +0400

    hhj

--- 0.1/controll-post/main_controll_post_linux.py
+++ 0.1/controll-post/main_controll_post_linux.py
@@ -91,7 +91,7 @@ class ServerMainPult:
             self.PORT = 1112
         else:
             self.HOST = '192.168.88.5'
-            self.PORT = 2267
+            self.PORT = 2268
             
             
         # настройка сервера
