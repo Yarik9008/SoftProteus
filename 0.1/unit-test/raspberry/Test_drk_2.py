17fbde099f81daf078f3c9eb540b0242aedabef2
Yarik9008 <iaroslav.muhatdinov@yandex.ru>
2022-03-24 22:14:39 +0400

    qwert

--- 0.1/unit-test/raspberry/Test_drk_2.py
+++ 0.1/unit-test/raspberry/Test_drk_2.py
@@ -11,11 +11,11 @@ class PWMControll:
         # инициализация моторов
         self.kit = ServoKit(channels=16)
 
-        self.drk0 = self.kit.servo[0]
+        self.drk0 = self.kit.servo[2]
         self.drk0.set_pulse_width_range(1100, 1950)
         self.drk1 = self.kit.servo[1]
         self.drk1.set_pulse_width_range(1100, 1950)
-        self.drk2 = self.kit.servo[2]
+        self.drk2 = self.kit.servo[0]
         self.drk2.set_pulse_width_range(1100, 1950)
         self.drk3 = self.kit.servo[3]
         self.drk3.set_pulse_width_range(1100, 1950)
