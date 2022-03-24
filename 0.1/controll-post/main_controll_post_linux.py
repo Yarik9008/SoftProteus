6ed3fa89ebca7254e7d91fd45e282421e290add8
Yarik9008 <iaroslav.muhatdinov@yandex.ru>
2022-03-24 17:01:16 +0400

    qwery

--- 0.1/controll-post/main_controll_post_linux.py
+++ 0.1/controll-post/main_controll_post_linux.py
@@ -91,7 +91,7 @@ class ServerMainPult:
             self.PORT = 1112
         else:
             self.HOST = '192.168.88.5'
-            self.PORT = 1228
+            self.PORT = 1229
             
             
         # настройка сервера
